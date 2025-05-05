212a7efeaaabad6a9b97742123b9dcb8d849fd98
guybracha <guy.bracha@gmail.com>
2025-05-05 15:52:28 +0300

    redesign

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-var modal = document.getElementById("myModal");
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("myModal");
+  const modalImg = document.getElementById("img01");
+  const captionText = document.getElementById("caption");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+  document.querySelectorAll(".design-img").forEach(img => {
+    img.addEventListener("click", function () {
+      modal.classList.add("show");
+      modalImg.src = this.src;
+      captionText.textContent = this.alt;
+    });
+  });
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  document.querySelector(".close").onclick = function () {
+    modal.classList.remove("show");
+  };
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
-
-console.log("Developed by Guy Bracha - 2023. Contact me by Email");
+  // סגירה גם בלחיצה על רקע שחור
+  modal.addEventListener("click", function (e) {
+    if (e.target === modal) modal.classList.remove("show");
+  });
+});
